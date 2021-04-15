34d0b25b9bc451f3d665fbbafbf39a0bdfb9bb02
Alexander Eist <nashesolnce@gmail.com>
2021-04-15 11:19:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,77 +1,65 @@
 <?php
-    // VARIABLES
-    $debug  = 0;                                                             // вкл/выкл флаг debug.
+    /////////////// VARIABLES ////////////////////////////////                  // RU comments
+    $debug  = 0;                                                                // вкл/выкл флаг debug.
     $debug2 = false;                                                            // вкл/выкл флаг debug2.
     $dir = "./";                                                                // По умолчанию сканировать будем текущей каталог. 
-    $scan = [];                                                      // Сканируем $dir в аррей $scan.                                                               
-    $html = "<div class='container'><div class='row'>";
-    $dis_files = array(my_name(),"1");                                                       // По-умолчанию разрешены все файлы.
-    $disabled_extentions = [];                                                   // По-умолчанию разрешены все форматы.
+    $scan = [];                                                                 // Будем сканировать $dir в аррей $scan.                                                               
+    $html = "<div class='container'><div class='row'>";                         // Здесь соберем весь HTML
+    $dis_files = array(my_name(),"1");                                          // Массив запрещенных файловых расширений. По-умолчанию разрешены все файлы.
+    $disabled_extentions = [];                                                   // Заголовок сайта. 
     $title = "Magic Stick Автопубликатор";
-    $dir   = './';
-    // FUNCTIONS
-    function my_name() {
-        GLOBAL $debug;
-       return  (basename(__FILE__));        
+    $dir   = './';                                                               // по умолчанию директорий - коренной директорий
+    ///////////// FUNCTIONS ///////////////////////////////////
+    function my_name() {                                                         //вычисляем себя и исключаем из списка файлов для публикации
+        GLOBAL $debug;                                                           // подключаемся к глобальным переменным
+       return  (basename(__FILE__));                                             // возвращаем собственное имя без пути 
     }
     //-------------------------------------------------//
-    function disabled($b) {
-        GLOBAL $dis_files,$debug;
-        if($debug){echo("<pre>" . __LINE__ . ": ");print_r($dis_files);echo("</pre>");}
-        $c = false;                                                             // $c возвращяет false что онзначает не запрещено.                            
-        if(in_array($b, $dis_files)) return true;                                      // in_array(mixed needle, array haystack, [bool strict])         
-        if(in_array(get_extension($b), $dis_files)) return true;            //substr(string string, int start, [int length]) 
-        return $c;                                                               //  strrpos($b, '.')
+    function disabled($b) {                                                       // получаем имя папки/файла возвращаем булеан запрещен или нет
+        GLOBAL $dis_files,$debug;                                                  // подключаемся к глобальным переменным
+        if($debug){echo("<pre>" . __LINE__ . ": ");print_r($dis_files);echo("</pre>");} // выводим наладочную информацию
+        $c = false;                                                                     // $c возвращяет false что онзначает не запрещено.                            
+        if(in_array($b, $dis_files)) return true;                                       // если запрещенный файл/папка выходим с подтверждением ДА
+        if(in_array(get_extension($b), $dis_files)) return true;                        // если расширение запрещено возращаем ДА
+        return $c;                                                                      //  возвращаем НЕТ, если прошли все фильтры 
     }
     //--------------------------------------------------
-    function get_extension($a='.htsccess'){
-      GLOBAL $disabled_extensions;
-     // extract($_REQUEST);
-      $posr   = strrpos($a, ".")+1;
-      $posl   = strpos($a,".")+1;                                            //
-      $len_   = strlen($a);
-      if($len_- $posr){
-        if($posl){
-         return substr($a, ($posr - $len_)); 
-        }else{
-         return substr($a, ($posr - $len_ + 1));
+    function get_extension($a='.htsccess'){                                       // функция принимает имя файла/папки возвращает расширение или пустую строку
+      GLOBAL $disabled_extensions;                                                // подключаемся к глобальной переменной
+     // extract($_REQUEST);                                                       // зарезервировано на случай использования технологии задания переменных в запросе  
+      $posr   = strrpos($a, ".")+1;                                               // позиция последней точки в полученном имени
+      $posl   = strpos($a,".")+1;                                                 //  позиция первой точки в позиции
+      $len_   = strlen($a);                                                       // длина имени файла/папки   
+      if($len_- $posr){                                                           // если длина больше позиции правой точки (если за последней точкой имеются символы) 
+        if($posl){                                                                // если вообще имеется хотя бы одна точка в имени
+         return substr($a, ($posr - $len_));                                      // возвращаем остаток имени после последней точки
+        }else{                                                                    // иначе если точек вообще нет
+         return substr($a, ($posr - $len_ + 1));                                  // возвращаем остаток имени длиной на единицу меньше чем позиция последней точки
         } 
       }
-      return  false;   // substr($a, die ("Extension Not Exists"));
+      return  false;                                                              // если прошли все фильтры возвращаем фальс
     }
 ////////////////// MAIN //////////////////////////////////////    
     
-     extract($_REQUEST);
-    $scan=scandir($dir)  ;
+     extract($_REQUEST);                                                         // распаковываем Запрос в переменные
+    $scan=scandir($dir)  ;                                                       // сканируем директорию
     if($debug2) {echo("<pre>"); print_r($scan);echo("</pre>"); }                 // Конструкция для проверки.
-    $disabled_files = my_name();                                                // Запрещяем работать с самими собой.
-    
-    
+    $disabled_files = my_name();                                                 // Запрещяем работать с самими собой.
     for($i=0;$i<count($scan); $i++) {                                           // Читаем содержимое каталога по-штучно.       
-       switch ($scan[$i]) {                                                     // Включаем фильтр для определение $nadpis.
-        case(".")  : $nadpis = "Повторить сканирование"; break;                  // Меняем . на Повторить сканирование для $nadpis.
-        case("..") : $nadpis = "Выйти из директории";   break;                  // Меняем .. на Выйти из директории для $nadpis.
-        default    : $nadpis = $scan[$i]; break;                                    // По-умолчанию берём имя файла.
+       switch ($scan[$i]) {                                                     // Включаем фильтр ключ селектор очередного имени файла/папки для определения надписи
+        case(".")  : $nadpis = "Повторить сканирование"; break;                 // Для директории "."  берем "Повторить сканирование" для $nadpis.
+        case("..") : $nadpis = "Выйти из директории";   break;                  // Для директории ".." берем "Выйти из директории" для $nadpis.
+        default    : $nadpis = $scan[$i]; break;                                // По-умолчанию        берём имя файла.
        }
        
-       if($i===0)    {$html  .= "<a href='"   . $scan[$i] . "'><div class='col-lg-6 place'>" . $nadpis . "</div></a>";
-       }elseif($i===1){$html .= "<a href='"   . $scan[$i] . "'><div class='col-lg-6 place'>" . $nadpis . "</div></a>"; 
+       if($i===0)    {$html  .= ;   //"<a href='"   . $scan[$i] . "'><div class='col-lg-6 place'>" . $nadpis . "</div></a>";  //составляем очередную строку ХТМЛ для "."    
+       }elseif($i===1){$html .= "<a href='"   . $scan[$i] . "'><div class='col-lg-6 place'>" . $nadpis . "</div></a>";  //составляем очередную строку ХТМЛ для ".."
        }else{
       if(!disabled($scan[$i])) $html                 .= "<a href='" . $scan[$i] . "'><div class='col-lg-2 col-md-3 col-sm-4 place'>" . $nadpis . "</div></a>" ;                  // Собираем html строку из содержимого.
        }
     }  
     $html    .= "</div></div>";
-   /*
-   <div class="card border-info mb-3" style="max-width: 20rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h4 class="card-title">Info card title</h4>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-   
-   
-   */ 
+  
     
 ?> 
 <head>
